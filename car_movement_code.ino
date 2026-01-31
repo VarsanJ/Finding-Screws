02126433f4e3728681372c98736bdf3bf42376fc
Ishraq Alam <ishraqalam23@gmail.com>
2026-01-31 13:46:33 -0500

    added more code to car movement

--- car_movement_code.ino
+++ car_movement_code.ino
@@ -10,6 +10,12 @@ void setup() {
 }
 
 void loop() {
-  
+  digitalWrite(leftWheel, HIGH);
+  digitalWrite(rightWheel, HIGH);
+
+  delay(1000);
+
+  digitalWrite(leftWheel, LOW);
+  digitalWrite(rightWheel, LOW);
     
 }
